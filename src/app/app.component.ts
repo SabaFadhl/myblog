4444656d1543e0e9505a5de03db0b15c342f62a3
SabaWesabi <s.fadhl@fintechsys.net>
2022-07-18 11:52:02 +0300

    Directives ngIf and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myblog';
+  myInfo=true;
+  myname="Saba Fadhl"
+  // Parameter type annotation
+  greet(name: string) {
+    console.log("Hello, " + name.toUpperCase() + "!!");
+  }
+   dataInfo:boolean=true;
+   customersArray:customer[]=[
+    {
+      name:"saba",
+      age:24,
+      city:"Dhamar"
+     },
+     {
+      name:"Rwan",
+      age:24,
+      city:"Sana'a"
+     },
+     {
+      name:"Hisham",
+      age:24,
+      city:"Taiz"
+     }
+
+   ];
+   personObject={
+    name:"saba",
+    age:24,
+    city:"Sana'a"
+   }
+}
+
+class customer{
+  name:string="";
+  age:number=0;
+  city:string="";
 }
